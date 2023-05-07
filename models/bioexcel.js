7c74238b88344a8dd0d7db88cb27de2b49263062
sundarajanThinkcove <sundarajan@thinkcove.com>
2023-05-07 14:37:20 +0530

    first changes

--- models/bioexcel.js
+++ models/bioexcel.js
@@ -4,8 +4,8 @@ const excelSchema = new mongoose.Schema({
     chapters: [{
         chapterId: { type: String },
         chapterName: { type: String },
-        
-        questions:[{
+
+        questions: [{
             questionId: { type: String },
             question: { type: String },
             choices: [],
@@ -13,10 +13,17 @@ const excelSchema = new mongoose.Schema({
             answer: { type: String },
             explanation: { type: String },
             boolAnswer: { type: Boolean, default: false },
-            difficulty:{type : String, default : "medium"}
+            difficulty: { type: String, default: "medium" },
 
-        }]  
+            explanationn: { type: String },
+            boolAnswerr: { type: Boolean, default: false },
+            difficultyy: { type: String, default: "medium" }
+
+        }]
     }]
 })
+
+//dfsdfjahjdfjkadfsgfsfgs
+//dfgshfssffffffffffffffffff
 
 module.exports = mongoose.model('ExcelBio', excelSchema);
